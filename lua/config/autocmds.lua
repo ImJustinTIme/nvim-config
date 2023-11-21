4c68e1aef7796f5cd677e489a5973ec37511ecc8
ImJustinTIme <jukuennen@gmail.com>
2023-11-21 12:02:30 -0500

    ok I finally figured out linting like 4 hours later :(

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Add any additional autocmds hereby
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,19 +1,11 @@
 return {
     "neovim/nvim-lspconfig",
     opts = {
-        servers = {
-            eslint = {},
-        },
+        servers = { eslint = {} },
         setup = {
             eslint = function()
                 require("lazyvim.util").lsp.on_attach(function(client)
                     client.server_capabilities.documentFormattingProvider = true
-
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        callback = function()
-                            require("lazyvim.util").lsp.formatting_sync()
-                        end,
-                    })
                 end)
             end,
         },
